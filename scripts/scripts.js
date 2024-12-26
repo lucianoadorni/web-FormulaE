fb2b62ba87ce63892ac9f39b8ff2d23b9dfadd12
lucianoadorni <lucianoadorni@hotmail.com>
2024-12-26 18:23:29 +0100

    FIxed and added stuff.
    Mostly fixed.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,37 @@
-(function () {
-    var scroll = new LocomotiveScroll();
-})();
+var textWrapper = document.querySelector('.ml13');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml13 .letter',
+    translateY: [100,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1400,
+    delay: (el, i) => 300 + 30 * i
+  }).add({
+    targets: '.ml13 .letter',
+    translateY: [0,-100],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1200,
+    delay: (el, i) => 100 + 30 * i
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#splide', {
+      type: 'loop',
+      perPage: 3,
+      perMove: 1,
+      gap: '1rem',
+      breakpoints: {
+        768: {
+          perPage: 2,
+        },
+        480: {
+          perPage: 1,
+        },
+      },
+    }).mount();
+  });
