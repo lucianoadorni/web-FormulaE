67dc63eb380c8be3b8837d4bfc8d66a4a7916379
lucianoadorni <142657836+lucianoadorni@users.noreply.github.com>
2025-01-03 13:46:13 +0100

    Deleted three and added more stuff.
    Noticed module-viewer does not need and so...

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -20,7 +20,7 @@ anime.timeline({loop: true})
 });
 
 
-document.addEventListener('DOMContentLoaded', function () {
+$(document).ready(function () {
   new Splide('#splide', {
     type: 'loop',
     perPage: 5,
@@ -46,6 +46,33 @@ document.addEventListener('DOMContentLoaded', function () {
   //   el: document.querySelector('[data-scroll-container]'),
   //   smooth: true
   // });
+
+// Set the date we're counting down to
+var countDownDate = new Date("Feb 14, 2025 15:37:25").getTime();
+
+// Update the countdown every 1 second
+var x = setInterval(function () {
+    // Get today's date and time
+    var now = new Date().getTime();
+
+    // Find the distance between now and the countdown date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes, and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    // Display the result in the element with id="demo"
+    $("#COUNTDOWN").html("JEDDAH E-PRIX STARTS IN " + days + "d " + hours + "h " + minutes + "m " + seconds + "s");
+
+    // If the countdown is finished, write some text
+    if (distance < 0) {
+        clearInterval(x);
+        $("#COUNTDOWN").html("NOW");
+    }
+}, 1000);
 });
 
 
